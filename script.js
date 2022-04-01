573751555fe2d0d4e86ff9e98d7d38173b068f65
Rohith209 <rohithsai209@gmail.com>
2022-04-01 21:42:50 +0530

    important

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=prompt("Please enter your Sweet First Name Birthday Girl!");
+ var b=prompt("Enter the date God sent you on this planet");
+  	if(a== "Mounika" && b=="02-04-2000")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
